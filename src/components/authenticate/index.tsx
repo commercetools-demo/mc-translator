13794a49108e21947d59fd8a0e4578715b389268
Philipp Hofmann <philipp.hofmann@commercetools.com>
2023-10-19 12:20:27 +0200

    using default components

--- src/components/authenticate/index.tsx
+++ src/components/authenticate/index.tsx
@@ -1,24 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import PrimaryButton from '@commercetools-uikit/primary-button';
-import Text from '@commercetools-uikit/text';
-import Spacings from '@commercetools-uikit/spacings';
-import React, { useEffect, useState } from 'react';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
 import messages from './messages';
 import { useIntl } from 'react-intl';
-import FlatButton from '@commercetools-uikit/flat-button';
-import { Link as RouterLink } from 'react-router-dom';
-import { BackIcon } from '@commercetools-uikit/icons';
+import { useHistory } from 'react-router-dom';
+import { InfoDetailPage } from '@commercetools-frontend/application-components';
 declare var gapi: any;
 
 type TAuthenticateProps = {
   linkToWelcome: string;
 };
 
-const Authenticate: React.FC<
-  { children: React.ReactNode } & TAuthenticateProps
-> = ({ children, linkToWelcome }) => {
+const Authenticate: React.FC<PropsWithChildren<TAuthenticateProps>> = ({
+  children,
+  linkToWelcome,
+}) => {
   const intl = useIntl();
+  const { push } = useHistory();
 
   // @ts-ignore
   const { googleClientID, translateApiKey } = useApplicationContext(
@@ -33,7 +31,7 @@ const Authenticate: React.FC<
         function () {
           console.log('GAPI client loaded for API');
         },
-        function (err: any) {
+        function (err: unknown) {
           console.error('Error loading GAPI client for API', err);
         }
       )
@@ -53,7 +51,7 @@ const Authenticate: React.FC<
           console.log('Sign-in successful');
           loadClient();
         },
-        function (err: any) {
+        function (err: unknown) {
           console.error('Error signing in', err);
         }
       );
@@ -67,34 +65,22 @@ const Authenticate: React.FC<
         plugin_name: 'hello-and', //any name can be used
       });
     });
-    // @ts-ignore
     console.log();
   }, [googleClientID]);
   return (
     <>
       {!isLoggedIn && (
-        <div>
-          <Spacings.Stack scale="xs">
-            <FlatButton
-              as={RouterLink}
-              to={linkToWelcome}
-              label={intl.formatMessage(messages.backToWelcome)}
-              icon={<BackIcon />}
-            />
-          </Spacings.Stack>
-          <Spacings.Stack scale="xs">
-            <Text.Headline as="h1" intlMessage={messages.title} />
-            <Text.Subheadline as="h4" intlMessage={messages.subtitle} />
-            <PrimaryButton
-              label={intl.formatMessage(messages.button)}
-              onClick={authenticate}
-              type="button"
-              tone="primary"
-              size="medium"
-              style={{ color: 'black', width: 'fit-content' }}
-            ></PrimaryButton>
-          </Spacings.Stack>
-        </div>
+        <InfoDetailPage
+          title={intl.formatMessage(messages.title)}
+          subtitle={intl.formatMessage(messages.subtitle)}
+          onPreviousPathClick={() => push(linkToWelcome)}
+          previousPathLabel={intl.formatMessage(messages.backToWelcome)}
+        >
+          <PrimaryButton
+            label={intl.formatMessage(messages.button)}
+            onClick={authenticate}
+          ></PrimaryButton>
+        </InfoDetailPage>
       )}
       {isLoggedIn && children}
     </>
--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,16 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useIntl } from 'react-intl';
-import {
-  Link as RouterLink,
-  useHistory,
-  useRouteMatch,
-} from 'react-router-dom';
-import { BackIcon } from '@commercetools-uikit/icons';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 import CheckboxInput from '@commercetools-uikit/checkbox-input';
-import FlatButton from '@commercetools-uikit/flat-button';
 import SecondaryButton from '@commercetools-uikit/secondary-button';
 import Spacings from '@commercetools-uikit/spacings';
-import Text from '@commercetools-uikit/text';
 import SelectField from '@commercetools-uikit/select-field';
 import messages from './messages';
 import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
@@ -25,12 +17,15 @@ import {
   useTranslateProducts,
 } from '../../hooks/use-products-connector';
 import LoadingSpinner from '@commercetools-uikit/loading-spinner';
-import DataTable from '@commercetools-uikit/data-table';
+import DataTable, { TColumn, TRow } from '@commercetools-uikit/data-table';
 import { Pagination } from '@commercetools-uikit/pagination';
-import styles from './products.module.css';
 import { useEffect, useMemo, useState } from 'react';
 import { useProductUpdater } from '../../hooks/use-products-connector/use-products-connector';
 import { ContentNotification } from '@commercetools-uikit/notifications';
+import {
+  InfoDetailPage,
+  PageContentWide,
+} from '@commercetools-frontend/application-components';
 
 type TProductsProps = {
   linkToWelcome: string;
@@ -77,7 +72,7 @@ const Products = (props: TProductsProps) => {
     countSelectedRows > 0 && countSelectedRows < rowsWithSelection.length;
   const handleSelectColumnHeaderChange =
     countSelectedRows === 0 ? selectAllRows : deselectAllRows;
-  const columns = [
+  const columns: Array<TColumn> = [
     {
       key: 'checkbox',
       label: (
@@ -88,7 +83,7 @@ const Products = (props: TProductsProps) => {
         />
       ),
       align: 'center',
-      renderItem: (row: any) => (
+      renderItem: (row: TRow) => (
         <CheckboxInput
           isChecked={getIsRowSelected(row.id)}
           onChange={() => toggleRow(row.id)}
@@ -106,7 +101,7 @@ const Products = (props: TProductsProps) => {
     [projectLanguages]
   );
 
-  const onTranslateProducts = async (rows: any[]) => {
+  const onTranslateProducts = async (rows: Array<TRow>) => {
     setIsLoading(true);
     const translatedProductsActions = await translateProductsActions(
       rows,
@@ -132,89 +127,90 @@ const Products = (props: TProductsProps) => {
     }
   }, [isTranslated]);
 
-  // @ts-ignore
   return (
-    <Spacings.Stack scale="xl">
-      <Spacings.Stack scale="xs">
-        <FlatButton
-          as={RouterLink}
-          to={props.linkToWelcome}
-          label={intl.formatMessage(messages.backToWelcome)}
-          icon={<BackIcon />}
-        />
-        <Text.Headline as="h2" intlMessage={messages.title} />
-      </Spacings.Stack>
-      <div className={styles.languageSelectorContainer}>
-        <SelectField
-          title={intl.formatMessage(messages.sourceLang)}
-          options={languageOptions}
-          onChange={(e) => setSourceLang(e.target.value as string)}
-          value={sourceLang}
-        />
-        <SelectField
-          title={intl.formatMessage(messages.destLang)}
-          options={languageOptions}
-          onChange={(e) => setDestLang(e.target.value as string)}
-          value={destLang}
-        />
-      </div>
-      {loading && <LoadingSpinner />}
+    <>
+      <InfoDetailPage
+        title={intl.formatMessage(messages.title)}
+        onPreviousPathClick={() => push(props.linkToWelcome)}
+        previousPathLabel={intl.formatMessage(messages.backToWelcome)}
+      >
+        <PageContentWide columns="1/1" gapSize={'10'}>
+          <SelectField
+            title={intl.formatMessage(messages.sourceLang)}
+            options={languageOptions}
+            onChange={(e) => setSourceLang(e.target.value as string)}
+            value={sourceLang}
+          />
+          <SelectField
+            title={intl.formatMessage(messages.destLang)}
+            options={languageOptions}
+            onChange={(e) => setDestLang(e.target.value as string)}
+            value={destLang}
+          />
+        </PageContentWide>
+      </InfoDetailPage>
+      <Spacings.Stack scale="s" alignItems={'stretch'}>
+        {loading && <LoadingSpinner scale={'s'} />}
 
-      {productsPaginatedResult ? (
-        <Spacings.Stack scale="l">
-          <DataTable<
-            NonNullable<
-              TFetchProductsQuery['productProjectionSearch']['results']
-            >[0]
-          >
-            isCondensed
-            columns={columns}
-            rows={rowsWithSelection}
-            itemRenderer={(item, column) => {
-              switch (column.key) {
-                case 'selected':
-                  return <CheckboxInput onChange={(e) => console.log(e)} />;
-                case 'id':
-                  return item.id;
-                case 'name':
-                  return item.name;
-                case 'nameDestination':
-                  return item.dstName;
-                default:
-                  return null;
+        {productsPaginatedResult ? (
+          <Spacings.Stack scale="s" alignItems="center">
+            <DataTable<
+              NonNullable<
+                TFetchProductsQuery['productProjectionSearch']['results']
+              >[0]
+            >
+              isCondensed
+              columns={columns}
+              rows={rowsWithSelection}
+              itemRenderer={(item, column) => {
+                switch (column.key) {
+                  case 'selected':
+                    return <CheckboxInput onChange={(e) => console.log(e)} />;
+                  case 'id':
+                    return item.id;
+                  case 'name':
+                    return item.name;
+                  case 'nameDestination':
+                    return item.dstName;
+                  default:
+                    return null;
+                }
+              }}
+              sortedBy={tableSorting.value.key}
+              sortDirection={tableSorting.value.order}
+              onSortChange={tableSorting.onChange}
+              onRowClick={(row) => push(`${match.url}/${row.id}`)}
+              footer={
+                <>
+                  {!isTranslated && (
+                    <SecondaryButton
+                      label="translate"
+                      iconLeft={
+                        isLoading ? <LoadingSpinner scale={'s'} /> : undefined
+                      }
+                      onClick={() => onTranslateProducts(rowsWithSelection)}
+                    />
+                  )}
+                  {isTranslated && (
+                    <ContentNotification type="success">
+                      {intl.formatMessage(messages.translated)}
+                    </ContentNotification>
+                  )}
+                </>
               }
-            }}
-            sortedBy={tableSorting.value.key}
-            sortDirection={tableSorting.value.order}
-            onSortChange={tableSorting.onChange}
-            onRowClick={(row) => push(`${match.url}/${row.id}`)}
-            footer={
-              <>
-                {!isTranslated && (
-                  <SecondaryButton
-                    label="translate"
-                    iconLeft={isLoading ? <LoadingSpinner /> : undefined}
-                    onClick={() => onTranslateProducts(rowsWithSelection)}
-                  />
-                )}
-                {isTranslated && (
-                  <ContentNotification type="success">
-                    {intl.formatMessage(messages.translated)}
-                  </ContentNotification>
-                )}
-              </>
-            }
-          />
-          <Pagination
-            page={page.value}
-            onPageChange={page.onChange}
-            perPage={perPage.value}
-            onPerPageChange={perPage.onChange}
-            totalItems={productsPaginatedResult.total}
-          />
-        </Spacings.Stack>
-      ) : null}
-    </Spacings.Stack>
+            />
+            <Pagination
+              page={page.value}
+              onPageChange={page.onChange}
+              perPage={perPage.value}
+              onPerPageChange={perPage.onChange}
+              totalItems={productsPaginatedResult.total}
+              perPageRange={'s'}
+            />
+          </Spacings.Stack>
+        ) : null}
+      </Spacings.Stack>
+    </>
   );
 };
 Products.displayName = 'Products';
