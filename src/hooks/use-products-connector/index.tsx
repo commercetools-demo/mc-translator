b82e745ccb09548610f49c2759ac43a3c30aa154
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-10-25 11:18:00 -0400

    remove duplicated files

--- src/hooks/use-products-connector/index.tsx
+++
@@ -1,4 +0,0 @@
-export { useProductsFetcher } from './use-products-connector';
-export { useTranslateProducts } from './translate-products';
-
-export type { TFetchProductsQuery } from './use-products-connector';
