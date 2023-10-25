c5673f3c64d5c728838e4119565a76b77d8b7844
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-10-25 10:55:28 -0400

    transform to connect app

---
+++ translator-mc-app/src/hooks/use-products-connector/index.tsx
@@ -0,0 +1,4 @@
+export { useProductsFetcher } from './use-products-connector';
+export { useTranslateProducts } from './translate-products';
+
+export type { TFetchProductsQuery } from './use-products-connector';
